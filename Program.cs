5aea3e07ef72b71a9c704659ec8c2606542b5b1d
Alam_Pranto <alampranto17@gmail.com>
2024-11-04 09:05:20 +0600

    new class update

--- Program.cs
+++ Program.cs
@@ -1,19 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace classworkofc_
 {
+    struct Point
+    {
+        public int x; // this is like java. you have to used access modifier every value.
+        public int y;
+
+
+        // C# is not working on this constructure 
+        //public point()
+        //{
+        //    this.x = 6; this.y = 0;
+        //}
+
+        public Point(int x, int y) // parameter constructure
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void print()
+        {
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+
+    };
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            
-            int a = 10, b = 20;
+
+            /*int a = 10, b = 20;
             Console.WriteLine("hello world");
             Console.Write("hello ,i am alam Pranto\n");
             Console.WriteLine($"a={a} b={b}");  // system 1 
@@ -51,11 +80,39 @@ namespace classworkofc_
             Console.WriteLine($"character : {z.KeyChar}");
             Console.WriteLine($"Modifer= {z.Modifiers}");
 
-            // Read key word --> assikey value
+            // Read word --> assikey value
 
             Console.WriteLine("enter a character");
             int arr = Console.Read();
-            Console.WriteLine($"character assikey value : {arr}");
+            Console.WriteLine($"character assikey value : {arr}");*/
+
+            //class 2 update 29-11-24
+
+            Point P;
+            P.x = 10;
+            P.y = 20;
+            Console.WriteLine($"X= {P.x} , y= {P.y}");
+
+            P = new Point(10, 12);
+
+            Console.WriteLine($"X= {P.x} , y= {P.y}");
+
+            Point P1= new Point(21, 12);
+            P1.print();
+
+            Point P3 = P1;  //p1 copy send to p3 but p1 will not affected
+            P3.x++;
+            P3.y++;
+
+            P3.print();
+            P3.x = Int32.Parse(Console.ReadLine());
+            P3.y= Int32.Parse(Console.ReadLine());
+            P3.print();
+
+
+            
+
+
 
 
 
